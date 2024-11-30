a9ef0d2af4d9bd56024d037ad2f76d9519afda32
sarafaqih <sarafaqih111@gmail.com>
2024-11-30 04:12:11 +0300

    lab solution

--- app.js
+++ app.js
@@ -1,121 +1,89 @@
 
 const buttons = document.querySelectorAll(".button")
-
+const numbers = document.querySelectorAll(".number")
 const calculatorDisplayElement = document.querySelector(".display")
+calculatorDisplayElement.textContent =0
+const calculator = document.querySelector('#calculator');
 
-buttons.forEach((button) => {
-    button.addEventListener('click', (event) => {
-      // This log is for testing purposes to verify we're getting the correct value
-      //console.log(event.target.innerText);
-      // Future logic to capture the button's value would go here...
-    });
-  });
 let num1=null
 let operator = null
 let num2 = null
 let total =0
-  const calculator = document.querySelector('#calculator');
-  calculator.addEventListener('click', (event) => {
-    // This log is for testing purposes to verify we're getting the correct value
-    // You have to click a button to see this log
-    //console.log(event.target.innerText);
+let setnumber=null
 
-    // Example
-    if(num1 === null){
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      if(num1===null || operator ===null){
       if(event.target.classList.contains('number')){
-      num1 = Number(event.target.innerText)
-      //console.log("i am num1 " +num1)
-      calculatorDisplayElement.textContent = num1    
+        calculatorDisplayElement.textContent = Number(calculatorDisplayElement.textContent + event.target.textContent)
+        num1 = calculatorDisplayElement.textContent
+      }
     }
-  }
-  })
-  
-        calculator.addEventListener('click', (event) => {
-          if(event.target.classList.contains('operator' )){
-            operator = event.target.innerText
-            //console.log("i am the operator "+ operator)
-            calculatorDisplayElement.textContent = 0
-            
-          }
-        })
-          calculator.addEventListener('click', (event) => {
-            if(event.target.classList.contains('number')){
-              num2 = Number(event.target.innerText)
-              //console.log("i am num2 "+ num2)
-              calculatorDisplayElement.textContent = num2
+      if(event.target.classList.contains('operator')){
+        operator = event.target.innerText
+        calculatorDisplayElement.textContent = 0
+      }
+      if(event.target.classList.contains('operatorc')){
+        num1 = null
+        num2=null
+        operator = null
+        total=0
+        calculatorDisplayElement.textContent = 0
 
-  
-            }
-            
-          })
-            calculator.addEventListener('click', (event) => {
-              if(event.target.classList.contains('equals')){
-                if(operator === '*'){
-                  total = num1 * num2
-                  //console.log(`The total is equal ${total}`)
-                  calculatorDisplayElement.textContent = total
-                  num1 = null
-                  num2=null
-                  operator = null
-                  total=0
+      }
+      if(num1 !== null && operator !== null ){
+      if(event.target.classList.contains('number')){
+        calculatorDisplayElement.textContent = Number(calculatorDisplayElement.textContent + event.target.textContent)
+        num2 = calculatorDisplayElement.textContent
+      }
+    }
+    if(event.target.classList.contains('equals')){
+      if(operator === '*'){
+        total = num1 * num2
+        //console.log(`The total is equal ${total}`)
+        calculatorDisplayElement.textContent = total
+        num1 = null
+        num2=null
+        operator = null
+        total=0
 
-                }
-                else if(operator === '/'){
-                  total = num1 / num2
-                  // console.log(`The total is equal ${total}`)
-                  calculatorDisplayElement.textContent = total
-                  num1 = null
-                  num2=null
-                  operator = null
-                  total=0
+      }
+      else if(operator === '/'){
+        total = num1 / num2
+        // console.log(`The total is equal ${total}`)
+        calculatorDisplayElement.textContent = total
+        num1 = null
+        num2=null
+        operator = null
+        total=0
 
-                }
-                else if(operator === '-'){
-                  total = num1 - num2
-                  // console.log(`The total is equal ${total}`)
-                  calculatorDisplayElement.textContent = total
-                  num1 = null
-                  num2=null
-                  operator = null
-                  total=0
+      }
+      else if(operator === '-'){
+        total = num1 - num2
+        // console.log(`The total is equal ${total}`)
+        calculatorDisplayElement.textContent = total
+        num1 = null
+        num2=null
+        operator = null
+        total=0
 
-                }
-                else if(operator === '+'){
-                  total = num1 + num2  
-                  // console.log(`The total is equal ${total}`)
-                  calculatorDisplayElement.textContent = total
-                  num1 = null
-                  num2=null
-                  operator = null
-                }
-                else{
-                  num1 = null
-                  num2=null
-                  operator = null
-                  total=0
+      }
+      else if(operator === '+'){
+        total = Number(num1) + Number(num2)
+        //console.log(`The total is equal ${total}`)
+        calculatorDisplayElement.textContent = total
+        num1 = null
+        num2=null
+        operator = null
+      }
+      else{
+        num1 = null
+        num2=null
+        operator = null
+        total=0
 
-                }
-                
-              }
-            
-              })
+      }
+    }
 
-              
-            
-      
-
-    // Example
-    // if (event.target.innerText === '*') {
-    //   // Do something with this operator
-    // }
-  //});
-  
-/*-------------------------------- Constants --------------------------------*/
-
-/*-------------------------------- Variables --------------------------------*/
-
-/*------------------------ Cached Element References ------------------------*/
-
-/*----------------------------- Event Listeners -----------------------------*/
-
-/*-------------------------------- Functions --------------------------------*/
+    });
+  });
